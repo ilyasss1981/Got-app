cabe09b561e09b6f6941fcf8ec528448642cb2fd
ILYAS NASIROV <ilyasss1981@gmail.com>
2021-01-01 01:04:03 +0300

    got-app lesson_98

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-binghkeqmx"><div className="ldio-h71a1v4y1j9">
+            <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+        </div></div>
+    )
+}
+
+export default Spinner
